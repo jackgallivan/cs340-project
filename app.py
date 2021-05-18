44b53fdf4cb2949095ba247a27312a8702f01042
richiestuver <richiestuver@protonmail.com>
2021-05-18 16:45:56 -0700

    implement database connection and provide a test query.

--- app.py
+++ app.py
@@ -6,9 +6,31 @@ Date Created: 05-18-21
 """
 
 from flask import Flask
+import mysql.connector
+from markupsafe import escape
+import os
+
+import database.db_connector as db
 
 app = Flask(__name__)
+db_connection = db.connect_to_database()
+
+
+"""Routes"""
 
 @app.route("/")
-def hello_world():
-    return "Hello world!"
+def test_db():
+    """
+    Test the db connection. Assumes that you have a database set up according to .env file and 
+    that database contains the table bsg_people. 
+    """
+    
+    query = "SELECT * FROM bsg_people;"
+    results = db.execute_query(db_connection=db_connection, query=query)
+
+    data = "<p>"
+    for row in results:
+        for field in row:
+            data += "<b>" + str(field) + "</b>" + ": " + str(row[field]) + " "
+        data += "</p>"
+    return data
--- database/db_connector.py
+++ database/db_connector.py
@@ -1,4 +1,4 @@
-import MySQLdb
+import mysql.connector
 import os
 from dotenv import load_dotenv, find_dotenv
 
@@ -15,14 +15,14 @@ def connect_to_database(host = host, user = user, passwd = passwd, db = db):
     '''
     connects to a database and returns a database objects
     '''
-    db_connection = MySQLdb.connect(host,user,passwd,db)
+    db_connection = mysql.connector.connect(host=host,user=user,password=passwd,db=db)
     return db_connection
 
 def execute_query(db_connection = None, query = None, query_params = ()):
     '''
     executes a given SQL query on the given db connection and returns a Cursor object
 
-    db_connection: a MySQLdb connection object created by connect_to_database()
+    db_connection: a mysql.connector connection object created by connect_to_database()
     query: string containing SQL query
 
     returns: A Cursor object as specified at https://www.python.org/dev/peps/pep-0249/#cursor-objects.
@@ -38,9 +38,9 @@ def execute_query(db_connection = None, query = None, query_params = ()):
         print("query is empty! Please pass a SQL query in query")
         return None
 
-    print("Executing %s with %s" % (query, query_params));
+    print("Executing %s with %s" % (query, query_params))
     # Create a cursor to execute query. Why? Because apparently they optimize execution by retaining a reference according to PEP0249
-    cursor = db_connection.cursor(MySQLdb.cursors.DictCursor)
+    cursor = db_connection.cursor(dictionary=True)
 
     '''
     params = tuple()
@@ -52,14 +52,15 @@ def execute_query(db_connection = None, query = None, query_params = ()):
     cursor.execute(query, query_params)
     # this will actually commit any changes to the database. without this no
     # changes will be committed!
-    db_connection.commit();
-    return cursor
+    results = cursor.fetchall()
+    db_connection.commit()
+    return results
 
 if __name__ == '__main__':
     print("Executing a sample query on the database using the credentials from db_credentials.py")
     db = connect_to_database()
     query = "SELECT * from bsg_people;"
-    results = execute_query(db, query);
+    results = execute_query(db, query)
     print("Printing results of %s" % query)
 
     for r in results.fetchall():
