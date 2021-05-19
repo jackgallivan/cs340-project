ea45ac5ac84d666c27f600d6c52da470411a8f22
jackgallivan <jackmgallivan@gmail.com>
2021-05-18 18:33:07 -0700

    Added routes, changed table header names

--- app.py
+++ app.py
@@ -23,22 +23,50 @@ def root():
     return render_template("index.j2")
 
 @app.route("/devices")
-def devices():
-    query = "SELECT deviceID, deviceName, dateLaunched, manufacturer, locationID, missionID FROM devices;"
+def devices_route():
+    query = "SELECT deviceID, deviceName, dateLaunched, manufacturer, locationName, missionName \
+            FROM devices \
+            JOIN locations ON devices.locationID = locations.locationID \
+            JOIN missions ON devices.missionID = missions.missionID;"
     results = db.execute_query(db_connection=db_connection, query=query)
     return render_template("devices.j2", data=results)
 
 @app.route("/functions")
-def functions():
-    query = "SELECT functionID, functionName, description FROM functions;"
+def functions_route():
+    query = "SELECT functionID, functionName, description \
+            FROM functions;"
     results = db.execute_query(db_connection=db_connection, query=query)
-    return render_template("devices.j2", data=results)
+    return render_template("functions.j2", data=results)
+
+@app.route("/device_function")
+def device_function_route():
+    query = "SELECT deviceName, functionName FROM device_function \
+            JOIN devices ON device_function.deviceID = devices.deviceID \
+            JOIN functions ON device_function.functionID = functions.functionID;"
+    results = db.execute_query(db_connection=db_connection, query=query)
+    return render_template("device_function.j2", data=results)
+
+@app.route("/missions")
+def missions_route():
+    query = "SELECT missionID, missionName, objective, locationName \
+            FROM missions JOIN locations ON missions.locationID = locations.locationID;"
+    results = db.execute_query(db_connection=db_connection, query=query)
+    return render_template("missions.j2", data=results)
 
 @app.route("/locations")
-def locations():
-    query = "SELECT locationID, locationName, localsystem, localBody FROM locations;"
+def locations_route():
+    query = "SELECT locationID, locationName, localsystem, localBody \
+            FROM locations;"
     results = db.execute_query(db_connection=db_connection, query=query)
     return render_template("locations.j2", data=results)
+
+@app.route("/operators")
+def operators_route():
+    query = "SELECT operatorID, operatorName, deviceName \
+            FROM operators \
+            JOIN devices ON operators.deviceID = devices.deviceID;"
+    results = db.execute_query(db_connection=db_connection, query=query)
+    return render_template("operators.j2", data=results)
 
 @app.route("/reset")
 def reset_db():
